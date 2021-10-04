ae589a5f41425527b03ce23cf487dc7fa02f219c
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-10-04 11:32:51 +0600

    ,

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,6 @@
 
 
 
-
-//banner animation
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  $('.page-banner-bg').css({
-    '-webkit-transform' : 'scale(' + (1 + scroll/2000) + ')',
-    '-moz-transform'    : 'scale(' + (1 + scroll/2000) + ')',
-    '-ms-transform'     : 'scale(' + (1 + scroll/2000) + ')',
-    '-o-transform'      : 'scale(' + (1 + scroll/2000) + ')',
-    'transform'         : 'scale(' + (1 + scroll/2000) + ')'
-  });
-});
-
-
 if($('.fancybox').length){
 $('.fancybox').fancybox({
     //openEffect  : 'none',
